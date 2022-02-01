8c8d3ec901898daa8c1e5e2504f31a80d158f1f0
Mateus Fontoura <fontourasantana@gmail.com>
2022-02-01 13:50:13 -0300

    fix: corrige bug da exceção lançada na conversão de string vazia para booleano

--- src/helpers/settings.py
+++ src/helpers/settings.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
-from distutils.util import strtobool
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
 load_dotenv()
 
-dry_run = strtobool(os.environ.get('DRY_RUN', 'false'))
+dry_run = str2bool(os.environ.get('DRY_RUN', 'false'))
 repo_path = os.environ.get('REPO_PATH', './')
 api_token = os.environ.get('SHORTCUT_TOKEN')
 telegram_chat_id = os.environ.get('TELEGRAM_CHAT_ID')
 telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-ignore_last_tag = strtobool(os.environ.get('IGNORE_LAST_TAG', 'false'))
+ignore_last_tag = str2bool(os.environ.get('IGNORE_LAST_TAG', 'false'))
 changelog_path = os.environ.get('CHANGELOG_PATH', 'CHANGELOG.md')
-telegram_send_diff = strtobool(os.environ.get('TELEGRAM_SEND_DIFF', 'false'))
+telegram_send_diff = str2bool(os.environ.get('TELEGRAM_SEND_DIFF', 'false'))
 telegram_title = os.environ.get('TELEGRAM_TITLE')
